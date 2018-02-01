49c293c80da54967a12cf338b7f726ee9620120b
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2018-02-01 12:38:23 -0200

    03. Elaborando uma solução de view

---
+++ alurabank/app/ts/models/Negociacoes.ts
@@ -0,0 +1,16 @@
+class Negociacoes {
+
+    private _negociacoes: Negociacao[] = [];
+
+
+    adiciona(negociacao: Negociacao) {
+
+        this._negociacoes.push(negociacao);
+    }
+
+
+    paraArray(): Negociacao[] {
+
+        return [].concat(this._negociacoes);
+    }
+}

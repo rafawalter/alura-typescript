b3b1c162963ea6e4ada62025fe5fc1860c80f8bd
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2018-02-05 13:33:53 -0200

    01. Namespace e modulos externos

--- alurabank/app/ts/app.ts
+++ alurabank/app/ts/app.ts
@@ -1,3 +1,5 @@
+import { NegociacaoController } from "./controllers/NegociacaoController";
+
 const controller = new NegociacaoController();
 // detesto comentários
 $('.form').submit(controller.adiciona.bind(controller));
